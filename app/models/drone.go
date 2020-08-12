504be693a61b2c1f565783106d3eb0a0cdca5152
Buntaro.Orita <buntaro.orita@gmail.com>
2020-08-12 19:16:02 +0900

    drone patrols

--- app/models/drone.go
+++ app/models/drone.go
@@ -1,10 +1,12 @@
 package models
 
 import (
+	"log"
 	"time"
 
 	"gobot.io/x/gobot"
 	"gobot.io/x/gobot/platforms/dji/tello"
+	"golang.org/x/sync/semaphore"
 )
 
 const (
@@ -14,14 +16,20 @@ const (
 
 type DroneManager struct {
 	*tello.Driver
-	Speed int
+	Speed        int
+	patrolSem    *semaphore.Weighted
+	patrolQuit   chan bool
+	isPatrolling bool
 }
 
 func NewDroneManager() *DroneManager {
 	drone := tello.NewDriver("8889")
 	manager := &DroneManager{
-		Driver: drone,
-		Speed:  DefaultSpeed,
+		Driver:       drone,
+		Speed:        DefaultSpeed,
+		patrolSem:    semaphore.NewWeighted(1),
+		patrolQuit:   make(chan bool),
+		isPatrolling: false,
 	}
 	work := func() {
 		//TODO
@@ -34,4 +42,48 @@ func NewDroneManager() *DroneManager {
 	go robot.Start()
 	time.Sleep(WaitDroneStartSec * time.Second)
 	return manager
+}
+
+func (d *DroneManager) Patrol() {
+	go func() {
+		isAcquire := d.patrolSem.TryAcquire(1)
+		if !isAcquire {
+			d.patrolQuit <- true
+			d.isPatrolling = false
+			return
+		}
+		defer d.patrolSem.Release(1)
+		d.isPatrolling = true
+		status := 0
+		t := time.NewTicker(3 * time.Second)
+		for {
+			select {
+			case <-t.C:
+				d.Hover()
+				switch status {
+				case 1:
+					log.Println("1 Forward")
+					d.Forward(d.Speed)
+				case 2:
+					log.Println("2 Right")
+					d.Right(d.Speed)
+				case 3:
+					log.Println("3 Backward")
+					d.Backward(d.Speed)
+				case 4:
+					log.Println("4 Left")
+					d.Left(d.Speed)
+				case 5:
+					log.Println("5 rest status")
+					status = 0
+				}
+				status++
+			case <-d.patrolQuit:
+				t.Stop()
+				d.Hover()
+				d.isPatrolling = false
+				return
+			}
+		}
+	}()
 }
--- main.go
+++ main.go
@@ -1,18 +1,21 @@
 package main
 
 import (
-	"log"
+	"time"
 
+	"github.com/bunorita/gotello/app/models"
 	"github.com/bunorita/gotello/config"
 	"github.com/bunorita/gotrading/utils"
 )
 
 func main() {
 	utils.LoggingSettings(config.Config.LogFile)
-	log.Println("test")
-
-	// droneManager := models.NewDroneManager()
-	// droneManager.TakeOff()
-	// time.Sleep(10 * time.Second)
-	// droneManager.Land()
+	droneManager := models.NewDroneManager()
+	droneManager.TakeOff()
+	time.Sleep(10 * time.Second)
+	droneManager.Patrol()
+	time.Sleep(30 * time.Second)
+	droneManager.Patrol()
+	time.Sleep(10 * time.Second)
+	droneManager.Land()
 }
